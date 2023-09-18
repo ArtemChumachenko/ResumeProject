9dcf27159d779697cbc1b16a2d392391fb8518f1
ArtemChumachenko <chumachenko.artem@icloud.com>
2023-09-18 00:48:56 -0400

    refactor checkWorkExperienceSection

--- cypress/e2e/checkWorkExperienceSection.cy.js
+++ cypress/e2e/checkWorkExperienceSection.cy.js
@@ -3,10 +3,9 @@ describe('Work Experience Section', () => {
       
         cy.visit('/');
         
-        cy.get('#work-experience').then(() => {
-          cy.contains('AAN Consulting Corporation, NY').should('be.visible');
-          cy.contains('FrikShun, Cary').should('be.visible');
-          cy.contains('ECCO Shoes, Moscow').should('be.visible');
-        });
+        cy.get('#work-experience') 
+
+        await cy.contains('AAN Consulting Corporation, NY').should('be.visible');
+       
   });
 });
