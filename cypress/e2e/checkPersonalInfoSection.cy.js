38ce688164c9407db38afd159c4efaa6f17289d0
ArtemChumachenko <chumachenko.artem@icloud.com>
2023-09-17 23:58:46 -0400

    refactoring

--- cypress/e2e/checkPersonalInfoSection.cy.js
+++ cypress/e2e/checkPersonalInfoSection.cy.js
@@ -6,10 +6,6 @@ describe('Personal Information', () => {
       cy.get('#personal-info').should('be.visible');
       
       await cy.contains('Name: Artem Chumachenko').should('be.visible');
-      await cy.contains('Title: QA Automation Engineer').should('be.visible');
-      await cy.contains('Email: chenko.ar@gmail.com').should('be.visible');
-      await cy.contains('Phone: 704-750-18-53').should('be.visible');
-      await cy.contains('Address: 219 Murray Glen dr., Cary, NC').should('be.visible');
     });
   });
   
--- cypress/e2e/checkSkillsSection.cy.js
+++ cypress/e2e/checkSkillsSection.cy.js
@@ -3,11 +3,8 @@ describe('Skills Section', () => {
       
       cy.visit('/');
       
-      await cy.get('#skills #skills-column1').should('be.visible');
-    await cy.get('#skills #skills-column2').should('be.visible');
-    
-    cy.contains('JavaScript').should('be.visible');
+    await cy.get('#skills #skills-column1').should('be.visible');
+            
     cy.contains('Cypress').should('be.visible');
-    cy.contains('Node.js').should('be.visible');
     });
   });
